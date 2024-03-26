ad89e19fd9b418998e11e7ffae1c3a471e238412
KuchkoraliyevNodirbek <nodirbekqochqarlaiyev@gmail.com>
2024-03-27 00:29:35 +0500

    reactda test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-function App() {
+const questions = [
+  {
+    title: 'Mustaqillik qachon qabul qilingan???', // Savol sarlavhası
+    variants: ['1990 yil', '1991yil ', '1992 yil'], // Variantlar
+    correct: 1, // To'g'ri variantning indeksi (0 dan boshlanadi)
+  },
+  {
+    title: '5*5-25+15=? ', // Savol sarlavhası
+    variants: ['20', '0', '15'], // Variantlar
+    correct: 2, // To'g'ri variantning indeksi (0 dan boshlanadi)
+  },
+  {
+    title: '9*9-80+9=?', // Savol sarlavhası
+    variants: [
+      '1',
+      '11',
+      '10',
+    ], // Variantlar
+    correct: 2, // To'g'ri variantning indeksi (0 dan boshlanadi)
+  },
+];
+
+function Result({ step, question, onClickVariant }) {
+  const percentage = Math.round((step / questions.length) * 100) // Foizni hisoblash
+  console.log(step)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="progress">
+      <div style={{ width: `${percentage}%` }} className="progress-bar"></div> {/* Progress bar */}
+      <h1>{question.title}</h1> {/* Savol sarlavhası */}
+      {console.log(question.title)}
+      <ul>
+        {
+          question.variants.map((text, index) =>
+            <li onClick={() => onClickVariant(index)} key={text}>{text}</li>) // Variantlarni ro'yxatga olish
+        }
+      </ul>
+    </div>
+  )
+}
+
+function Javoblar({ correct }) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" /> {/* Rasm */}
+      <h2>siz {correct} ta tog'ri javob berdingiz {questions.length} ta savoldan </h2> {/* Natija matni */}
+      <a href="/"><button>Yana bir bor urinib korish..</button></a> {/* Qayta boshlash tugmasi */}
     </div>
   );
 }
 
-export default App;
+function App() {
+  const [step, setStep] = React.useState(0); // Qadam soni holati
+  const [correct, setCorrect] = React.useState(0); // To'g'ri javoblar soni holati
+
+  const question = questions[step]; // Joriy savol
+
+  const onClickVariant = (index) => {
+    setStep(step + 1) // Qadam sonini bir oshirish
+    if (index === question.correct) {
+      setCorrect(correct + 1) // To'g'ri javoblar sonini bir oshirish
+    }
+  }
+
+  return (
+    <div className="App">
+      {
+        step !== questions.length ? <Result step={step} question={question} onClickVariant={onClickVariant} /> :
+          <Javoblar correct={correct} /> // Qadam soni savollar soniga teng bo'lsa natijalarni ko'rsatish
+      }
+    </div>
+  )
+}
+
+export default App
